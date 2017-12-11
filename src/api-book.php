5c0c7cd278a73ae1c7757bce1da7aaf93ec7af15
qinnianchao <46673405@qq.com>
2017-12-11 17:07:20 +0800

    init

---
+++ src/api-book.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: apiBook
+ * Date: 2017/12/11
+ * Time: 下午5:01
+ * Company: HsereTech Co.,Ltd.
+ * Created: qinnianchao
+ */
+namespace Hsere\ApiBook;
+
+Class Test
+{
+    public function hello() {
+        var_dump('hello');
+    }
+}
